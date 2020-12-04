bfaaef8b3c872d7297e6121da3feaedf27ef2cc1
eltonorudzhov <qwetyel@gmailc.com>
2020-12-04 09:43:40 +0300

    first global homework

---
+++ lib/models/productModel.dart
@@ -0,0 +1,12 @@
+class ProductModel{
+  final String title;
+  final String subtitle;
+  final String price;
+  final String info;
+  final String protein;
+  final String fats;
+  final String carbohydrates;
+  final String calories;
+  final String imgUrl;
+
+  ProductModel(this.title, this.subtitle, this.price, this.info, this.protein, this.fats, this.carbohydrates, this.calories, this.imgUrl);}
